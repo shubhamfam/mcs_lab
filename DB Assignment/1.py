9db1fb7aefa2d8e0f60641e65833995f254b3174
shubhamfam <gaikwad.shubham1311@gmail.com>
2022-01-21 20:24:18 +0530

    added new assignments

---
+++ DB Assignment/1.py
@@ -0,0 +1,10 @@
+'''1.Write a Python program to update a specific column value of a given table and select all rows before 
+and after updating the said table. '''
+
+import db
+
+conn = db.connect("assignment.db")
+
+
+
+conn.close()
